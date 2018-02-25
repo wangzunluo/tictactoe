6f79002420dcba5381b21856971210c2e634ca48
wangzunluo <wangarwin13@gmail.com>
2018-02-24 21:52:49 -0600

    changed buttons to links

---
+++ tttFunctions.py
@@ -0,0 +1,19 @@
+def checkHorizontal():
+
+def checkVertical():
+
+def checkDiagonal():
+
+def checkFull(board):
+  for row in board:
+    for cell in row:
+      if not board[row][cell]
+        return False
+  return True
+
+def check
+def checkStatus(board, user, x, y):
+  [0,1], [1,0], [1,2], [2,1]
+  isEdge = checkEdge(board, x, y):
+
+  full = checkFull(board)
