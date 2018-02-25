9c12537e982cbb51aa33426a3dbd55aee2d64c3d
wangzunluo <wangarwin13@gmail.com>
2018-02-24 22:43:55 -0600

    tttFunctions

--- tttFunctions.py
+++ tttFunctions.py
@@ -1,8 +1,55 @@
 def checkHorizontal(board, user, x, y):
-
+  if user == 'X':
+    for cell in board[x]:
+      if board[x][cell] != 'X'
+        return False
+    return True
+  else
+    for cell in board[x]:
+      if board[x][cell] != 'Y'
+        return False
+    return True
 def checkVertical(board, user, x, y):
-
+  if user == 'X':
+    for row in board:
+      if board[row][y] != 'X'
+        return False
+    return True
+  else
+    for row in board:
+      if board[row][y] != 'Y'
+        return False
+    return True
 def checkDiagonal(board, user, x, y):
+  if x == 0:
+    if y == 0:
+      for i in range(1):
+        if not board[i][i] == board[i+1][i+1]:
+          return False
+      return True
+    else:
+      if not board[0][2] == board[1][1]:
+        return False
+      elif not board[2][0] == board[1][1]:
+        return False
+      return True
+  elif x == 2:
+    if y == 0:
+      if not board[0][2] == board[1][1]:
+        return False
+      elif not board[2][0] == board[1][1]:
+        return False
+      return True
+    else:
+      for i in range(1):
+        if not board[i][i] == board[i+1][i+1]:
+          return False
+      return True
+  else:
+    for i in range(1):
+      if not board[i][i] == board[i+1][i+1]:
+        return False
+    return True
 
 def checkFull(board):
   for row in board:
@@ -29,13 +76,23 @@ def checkEdge(board, x, y):
       return False
 
 def checkStatus(board, user, x, y):
-  isEdge = checkEdge(board, x, y):
+  isEdge = checkEdge(board, x, y)
+  result = [False,False,False]
+  if(user == 'X'):
+    user = 0
+  else:
+    user = 1
+  
   if not isEdge:
     if checkDiagonal(board, user, x, y):
-      return "game was won"
-  if checkHorizontal(board, user, x, y):
-    return "game was won"
+      result[user] = True
+      return result
+  if user = checkHorizontal(board, user, x, y):
+    result[user] = True
+    return result
   if checkVertical(board, user, x, y):
-    return "game was won"
-  full = checkFull(board)
-    return "cat game"
+    result[user] = True
+    return result
+  if checkFull(board):
+    result[2] = True
+    return result
