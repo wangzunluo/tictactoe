1f0497a34aaa4e35da5f06e61b7e6401faedae05
wangzunluo <wangarwin13@gmail.com>
2018-02-25 10:17:10 -0600

    testing ping

--- server.py
+++ server.py
@@ -14,6 +14,11 @@ global gameBoard
 playerNumber = 0
 gameBoard = [['','',''],['','',''],['','','']]
 
+def parseSummary(summary):
+    splitSummary = summary.split(" = ")
+    data = splitSummary[0].split("/")
+    values = splitSummary[1].split("/")
+    return [data, values]
 
 app = Flask(__name__, template_folder="./Templates")
 @app.route("/")
@@ -80,7 +85,12 @@ def ping():
     # data = data.strip(data_crap[1])
     # data = data.strip(data_crap[2])
     # data2 = data.strip(data_crap[3])
-    return data_crap[3]
+    parsedStrings = parseSummary(data)
+    data = parsedStrings[0]
+    values = parsedStrings[1]
+    return render_template("pingpage.html", rtm = data[0], 
+    rtmd = values[0], avg = data[1], avgv = values[1], max = data[2], 
+    maxv = values[2], sd = data[3], sdv = values[3])
 @app.route('/main')
 def resetGame():
     global playerNumber
